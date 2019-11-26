4ebce7979c24a456f0c08888259c209895eec190
Gene Rush <erush91@gmail.com>
2019-11-26 10:21:40 -0700

    Now can read in 16 bytes of data. Should be able to read in 544 bytes according to documentation though. Need to resolve.

--- src/main.cpp
+++ src/main.cpp
@@ -23,33 +23,37 @@
 
 void setup() 
 {
+  Serial.begin(115200);
   HWSERIAL.begin(115200, SERIAL_8N1);
 }
 
 void loop() 
 {
-  byte TxMsg[6] = {255, 8, 36, 0, 255, 254}; // Retrieve Device ID
-  int RxMsg[4];
+  // byte TxMsg[6] = {255, 8, 36, 0, 255, 254}; // Retrieve Device ID
+  byte TxMsg[6] = {255, 8, 36, 9, 255, 254}; // Retrieve Sensor Data
+  int RxMsg[16];
 
   HWSERIAL.write(TxMsg, 6);
 
-	while(HWSERIAL.available() < 4)
-  {
-    Serial.println("WAITING FOR 544 bytes!");
-  }
-  if(HWSERIAL.available() < 4)
+  // Wait until have entire data packet
+	while(HWSERIAL.available() < 16)
+  {}
+
+  if(HWSERIAL.available() < 16)
   {
     // the data didn't come in - handle that problem here
-    Serial.println("ERROR - Didn't get 544 bytes of data!");
+    Serial.println("?");
   }
   else
   {
-    // Serial.println("GOT THE DATA!");
-    for(int n=0; n<4; n++)
+    Serial.println("data");
+    for(int n=0; n<16; n++)
       {
       RxMsg[n] = HWSERIAL.read(); // Then: Get them.
-      Serial.print("UART received:");
       Serial.println(RxMsg[n]);
     }
+    Serial.clear();
+    HWSERIAL.clear();
+
   }
 }
