93327ef86d1cb6f3f11eeb6e8a3ae15ec6a141a1
Gene Rush <erush91@gmail.com>
2019-11-26 00:30:30 -0700

    works infrequently.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 //#include <SD.h>
 // #include <SPI.h>
 #include "Wire.h"
+// #include <centeye.h>
 
 // https://www.pjrc.com/teensy/td_uart.html
 // https://www.pjrc.com/teensy/td_uart.html
@@ -21,35 +22,68 @@
 // set this to the hardware serial port you wish to use
 #define HWSERIAL Serial1
 
+const byte numChars = 4;
+int RxMsg[numChars];
+
+boolean newData = false;
+static int ndx = 0;
+
+void getData();
+void printData();
+
 void setup() 
 {
+  Serial.begin(115200);
   HWSERIAL.begin(115200, SERIAL_8N1);
 }
 
 void loop() 
 {
+  getData();
+  printData();
+}
+
+void getData()
+{
+    // FOR DEVICE ID RETRIEVAL
   byte TxMsg[6] = {255, 8, 36, 0, 255, 254}; // Retrieve Device ID
-  int RxMsg[4];
+
+  // FOR DATA RETRIEVAL  
+  // byte TxMsg[6] = {255, 8, 36, 9, 255, 254}; // Retrieve Sensor Data
 
   HWSERIAL.write(TxMsg, 6);
 
-	while(HWSERIAL.available() < 4)
+  while (HWSERIAL.available() > 0 && newData == false)
   {
-    Serial.println("WAITING FOR 544 bytes!");
+    if (ndx < numChars)
+    {
+      RxMsg[ndx] = HWSERIAL.read();
+      ndx++;
+    }
+    else
+    {
+      ndx = 0;
+      newData = true;
+    }
   }
-  if(HWSERIAL.available() < 4)
+}
+
+void printData()
+{
+  if (newData == true)
   {
-    // the data didn't come in - handle that problem here
-    Serial.println("ERROR - Didn't get 544 bytes of data!");
-  }
-  else
-  {
-    // Serial.println("GOT THE DATA!");
-    for(int n=0; n<4; n++)
-      {
-      RxMsg[n] = HWSERIAL.read(); // Then: Get them.
-      Serial.print("UART received:");
+    // Serial.print("This just in ... ");
+    // for
+    // Serial.println(RxMsg);
+  
+    for(int n=0; n<1; n++)
+    {
       Serial.println(RxMsg[n]);
     }
+
+    newData = false;
+    Serial.flush();
+    HWSERIAL.flush();
+
   }
 }
