f3738aad0cea5f64499198ef4d0fa229d12d410d
Gene Rush <erush91@gmail.com>
2019-11-26 18:31:56 -0700

    Can read Device ID but not sensor data.

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include "Wire.h"
 
 // https://www.pjrc.com/teensy/td_uart.html
-// https://www.pjrc.com/teensy/td_uart.html
 // https://forum.pjrc.com/threads/43085-choosing-a-serial-port-on-the-Teensy-3-6
 // https://forum.pjrc.com/threads/55342-Teensy3-6-Serial-comm-error?p=199937#post199937
 
@@ -29,31 +28,34 @@ void setup()
 
 void loop() 
 {
-  // byte TxMsg[6] = {255, 8, 36, 0, 255, 254}; // Retrieve Device ID
-  byte TxMsg[6] = {255, 8, 36, 9, 255, 254}; // Retrieve Sensor Data
-  int RxMsg[16];
+  byte TxMsg[6] = {255, 8, 36, 0, 255, 254}; // Retrieve Device ID
+  // byte TxMsg[6] = {255, 8, 36, 9, 255, 254}; // Retrieve Sensor Data
+
+  int RxMsg;
+  unsigned int bytecount = 0;
 
   HWSERIAL.write(TxMsg, 6);
 
-  // Wait until have entire data packet
-	while(HWSERIAL.available() < 16)
-  {}
+  delay(3); // Need 3ms delay before reading Device ID (otherwise first byte = 252)
 
-  if(HWSERIAL.available() < 16)
+  while(HWSERIAL.available() && bytecount < 4)
   {
-    // the data didn't come in - handle that problem here
-    Serial.println("?");
-  }
-  else
-  {
-    Serial.println("data");
-    for(int n=0; n<16; n++)
-      {
-      RxMsg[n] = HWSERIAL.read(); // Then: Get them.
-      Serial.println(RxMsg[n]);
-    }
-    Serial.clear();
-    HWSERIAL.clear();
+
+      RxMsg = HWSERIAL.read();
+      Serial.print(RxMsg);
+      // Serial.print(bytecount);
+      // Serial.print(HWSERIAL.available());
+      Serial.print(", ");
+
+      bytecount++;
+
+      HWSERIAL.write(TxMsg, 6);
 
   }
+
+  Serial.println();
+
+  Serial.clear();
+  HWSERIAL.clear();
+
 }
