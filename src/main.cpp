9e282ba41a7f314c5b2294f4eca9c81275dda394
Gene Rush <erush91@gmail.com>
2019-12-06 14:34:37 -0700

    Now reads all 544 bytes to serial monitor, thanks to support from Geof. I was writing the Tx command after every byte read, but should have been writing only once every loop. Also, Modified the delay after each byte read, 80-90us works best. Geof also helped provide code to assign data to a pointer, and point  specific variables to the pointer.

--- src/main.cpp
+++ src/main.cpp
@@ -28,34 +28,83 @@ void setup()
 
 void loop() 
 {
-  byte TxMsg[6] = {255, 8, 36, 0, 255, 254}; // Retrieve Device ID
-  // byte TxMsg[6] = {255, 8, 36, 9, 255, 254}; // Retrieve Sensor Data
+  // byte TxMsg[6] = {255, 8, 36, 0, 255, 254}; // Retrieve Device ID
+  byte TxMsg[6] = {255, 8, 36, 9, 255, 254}; // Retrieve Sensor Data // Dataset 9
+  // byte TxMsg[6] = {255, 8, 36, 3, 255, 254}; // Retrieve Sensor Data // Dataset 3
 
   int RxMsg;
   unsigned int bytecount = 0;
+  unsigned char Data[544];
 
   HWSERIAL.write(TxMsg, 6);
 
-  delay(3); // Need 3ms delay before reading Device ID (otherwise first byte = 252)
+  Serial.println();
+  Serial.print("NEW DATA");
+  Serial.println();
 
-  while(HWSERIAL.available() && bytecount < 4)
+  // Buffer of UART probably small, this makes sure buffer has some bytes
+  delay(1); // Need 3ms delay before reading Device ID (otherwise first byte = 252)
+
+  while(HWSERIAL.available() && bytecount < 544)
   {
 
-      RxMsg = HWSERIAL.read();
-      Serial.print(RxMsg);
-      // Serial.print(bytecount);
-      // Serial.print(HWSERIAL.available());
-      Serial.print(", ");
+    RxMsg = HWSERIAL.read();
+    Serial.print(RxMsg);
+    Data[bytecount] = RxMsg;
+    Serial.print(", ");
 
-      bytecount++;
-
-      HWSERIAL.write(TxMsg, 6);
-
+    bytecount++;
+    
+    // Delay must be short to prevent buffer from overflowing, but long enough for a new byte to come in.
+    delayMicroseconds(90);
+  
   }
+
+  // Enhance readability in serial monitor
+  delay(1000);
 
   Serial.println();
 
   Serial.clear();
   HWSERIAL.clear();
+
+  /////////////////////////////
+  // ASSIGN DATA TO POINTERS //
+  /////////////////////////////
+
+  // Int = 32 Bytes
+  int *p_int = (int*)Data;
+
+  // Short Int = 16 Bytes
+  short *p_short = (short*)Data;
+
+  //////////////////////////////
+  // ASSIGN DATA TO VARIABLES //
+  //////////////////////////////
+
+  // GLOBAL_X (4-Byte Long Integer)
+  int global_x = p_int[0];
+
+  // GLOBAL_Y (4-Byte Long Integer)
+  int global_y = p_int[1];
+
+  // x and y window optical flow odometries
+  int x_of[25],y_of[25];
+
+  // stereo distance and confidence flags for windows
+  short s_dist[25],s_conf[25];
+
+  // Note: window 0 does not have any information
+  for (int w = 0; w < 25; ++w) 
+  {
+    
+    int *p_wint = (int*)(Data + w * 20 + 44);
+    short *p_wshort = (short*)(Data + w * 20 + 44);
+    x_of[w] = p_wint[0];
+    y_of[w] = p_wshort[1];
+    s_dist[w] = p_wshort[4];
+    s_conf[w] = p_wshort[5];
+
+  }
 
 }
